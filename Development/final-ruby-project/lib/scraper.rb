40f8c0db3097fb2af5d7c3a3f1baa2b07d3662e3
babyshoes <dianecai@gmail.com>
2015-08-11 12:35:38 -0400

    Change scraper with... programming

--- Development/final-ruby-project/lib/scraper.rb
+++ Development/final-ruby-project/lib/scraper.rb
@@ -5,7 +5,21 @@ require 'pry'
 html = open('http://greatist.com/move/common-yoga-poses')
 file = Nokogiri::HTML(html)
 
-difficulty = file.css(".slideshow h3")
-title = file.css(".title-wrapper h4").first.text.strip
+def read_difficulties
+  difficulties = file.css(".slideshow h3")
+  difficulties = difficulties.collect {|difficulty| difficulty.text}
+end
+
+def read_titles
+  titles = file.css(".title-wrapper h4")
+  titles = titles.collect {|title| title.text.strip }
+end
+
+def read_instructions
+  instructions = file.css(".slide-body p:first-child")
+  instructions = instructions.collect {|instruction| instruction.text}
+end
+
+
 instructions = file.css(".slide-body p:first-child").first.text
 benefits = file.css(".slide-body p:nth-child(2)").first.text
